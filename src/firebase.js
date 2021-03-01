2458969d661e739e9394596a2944ec3931d923d5
Gozel Cholukova <gozelr@mail.ru>
2021-03-02 00:48:34 +0500

    Add files via upload

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCj7ahW9Yy7rCi1M5nZRIE2BYmprnK5Tpw",
   authDomain: "slack-clone-3512b.firebaseapp.com",
@@ -6,3 +8,10 @@ const firebaseConfig = {
   messagingSenderId: "87839488338",
   appId: "1:87839488338:web:d2fe3ee93fcda39951e28b"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider, db };
