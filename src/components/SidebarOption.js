e964416392b2c1c9f94b431043de44333c5b52b0
Gozel Cholukova <gozelr@mail.ru>
2021-03-02 00:49:09 +0500

    Add files via upload

--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import styled  from "styled-components";
+import { db } from '../firebase';
 
-function SidebarOption({ Icon, title}) {
+function SidebarOption({ Icon, title, addChannelOption}) {
+  const addChannel = () => {
+    const channelName = prompt("Please enter the channel name");
+
+    if (channelName) {
+      db.collection("rooms").add({
+        name: channelName,
+      });
+    }
+  };
+
+  const selectChannel = () => {};
+
   return (
-    <SidebarOptionContainer>
+    <SidebarOptionContainer
+      onClick={addChannelOption ? addChannel : selectChannel}
+    >
       {Icon && <Icon fontSize="small" style={{ padding: 10 }} />}
       {Icon ? (
         <h3>{title}</h3>
@@ -23,6 +38,20 @@ const SidebarOptionContainer = styled.div`
   font-size: 12px;
   align-items: center;
   padding-left: 2px;
+  cursor: pointer;
+  
+  :hover {
+    opacity: 0.9;
+    background-color: #340e36;
+  }
+
+  > h3 {
+    font-weight: 500;
+  }
+
+  > h3 > span {
+    padding: 15px;
+  }
 `;
 
 const SidebarOptionChannel = styled.div``;
