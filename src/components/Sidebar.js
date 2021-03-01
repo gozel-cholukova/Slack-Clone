27e9f69c63bfe9d5b62e245c4cf5b9c88249fcc7
Gozel Cholukova <gozelr@mail.ru>
2021-03-01 23:35:10 +0500

    Add files via upload

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import styled  from "styled-components";
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+import InsertCommentIcon from '@material-ui/icons/InsertComment';
+import InboxIcon from '@material-ui/icons/Inbox';
 import CreateIcon from '@material-ui/icons/Create';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
+import AppsIcon from '@material-ui/icons/Apps';
+import FileCopyIcon from '@material-ui/icons/FileCopy';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import SidebarOption from './SidebarOption';
 
 function Sidebar() {
   return (
@@ -16,6 +25,16 @@ function Sidebar() {
         </SidebarInfo>
         <CreateIcon />
       </SidebarHeader>
+
+      <SidebarOption Icon={InsertCommentIcon} title="Threads" />
+      <SidebarOption Icon={InboxIcon} title="Mentions & reactions" />
+      <SidebarOption Icon={DraftsIcon} title="Saved items" />
+      <SidebarOption Icon={BookmarkBorderIcon} title="Channel browser" />
+      <SidebarOption Icon={PeopleAltIcon} title="People & user groups" />
+      <SidebarOption Icon={AppsIcon} title="Apps" />
+      <SidebarOption Icon={FileCopyIcon} title="File browser" />
+      <SidebarOption Icon={ExpandLessIcon} title="Show less" />
+
     </SidebarContainer>
   );
 }
