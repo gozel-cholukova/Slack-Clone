ace463a52c352cd3f9f01e2788c04894de4fb4df
Gozel Cholukova <gozelr@mail.ru>
2021-03-01 23:36:51 +0500

    Add files via upload

---
+++ src/components/SidebarOption.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled  from "styled-components";
+
+function SidebarOption({ Icon, title}) {
+  return (
+    <SidebarOptionContainer>
+      {Icon && <Icon fontSize="small" style={{ padding: 10 }} />}
+      {Icon ? (
+        <h3>{title}</h3>
+      ) : (
+        <SidebarOptionChannel>
+          <span>#</span>{title}
+        </SidebarOptionChannel>
+      )}
+    </SidebarOptionContainer>
+  );
+}
+
+export default SidebarOption;
+
+const SidebarOptionContainer = styled.div`
+  display: flex;
+  font-size: 12px;
+  align-items: center;
+  padding-left: 2px;
+`;
+
+const SidebarOptionChannel = styled.div``;
