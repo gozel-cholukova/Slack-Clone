37a0781ece6bdcd559cf3250b455d18776127051
Gozel Cholukova <gozelr@mail.ru>
2021-03-06 02:36:47 +0500

    Add files via upload

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import StarBorderIcon from '@material-ui/icons/StarBorder';
 import InfoIcon from '@material-ui/icons/Info';
 import { useSelector } from 'react-redux';
 import { selectRoomId } from '../features/appSlice';
 import ChatInput from './ChatInput';
+import { useCollection, useDocument } from 'react-firebase-hooks/firestore';
+import { db } from '../firebase';
+import Messages from './Messages';
 
 function Chat() {
+  const chatRef = useRef(null);
   const roomId = useSelector(selectRoomId);
+  const [roomDetails] = useDocument(
+    roomId && db.collection("rooms").doc(roomId)
+  );
+  const [roomMessages, loading] = useCollection(
+    roomId && 
+      db
+        .collection("rooms")
+        .doc(roomId)
+        .collection("messages")
+        .orderBy("timestamp", "asc")
+  );
+
+  useEffect(() => {
+    chatRef?.current?.scrollIntoView({
+      behavior: "smooth",
+    });
+  }, [roomId, loading])
 
   return (
     <ChatContainer>
@@ -15,7 +36,7 @@ function Chat() {
         <Header>
           <HeaderLeft>
             <h4>
-              <strong>#Room-name</strong>
+              <strong>#{roomDetails?.data().name}</strong>
             </h4>
             <StarBorderIcon />
           </HeaderLeft>
@@ -28,21 +49,38 @@ function Chat() {
         </Header>
 
         <ChatMessages>
+          {roomMessages?.docs.map((doc) => {
+            const { message, timestamp, user, userImage } = doc.data();
 
+            return (
+              <Messages
+                key={doc.id}
+                message={message}
+                timestamp={timestamp}
+                user={user}
+                userImage={userImage}
+              />
+            );
+          })}
+          <ChatBottom ref={chatRef} />
         </ChatMessages>
 
         <ChatInput 
+          chatRef={chatRef}
+          channelName={roomDetails?.data().name}
           channelId={roomId}
         />
 
       </>
-    </ChatContainer>
-      
-    
+    </ChatContainer>  
   )
 }
 
 export default Chat;
+
+const ChatBottom = styled.div`
+  padding-bottom: 200px;
+`;
 
 const ChatContainer = styled.div`
   flex: 0.7;
@@ -90,3 +128,4 @@ const HeaderRight = styled.div`
 const ChatMessages = styled.div`
 
 `;
+
--- src/components/ChatInput.js
+++ src/components/ChatInput.js
@@ -4,20 +4,28 @@ import { Button } from "@material-ui/core";
 import { db } from '../firebase';
 import firebase from 'firebase';
 
-function ChatInput({channelName, channelId}) {
+function ChatInput({channelName, channelId, chatRef}) {
   const [input, setInput] = useState("");
 
   const sendMessage = (e) => {
     e.preventDefault();
 
-    if (channelId) {
+    if (!channelId) {
       return false;
     }
 
     db.collection('rooms').doc(channelId).collection('messages').add({
       message: input,
       timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-    });    
+      user: "Gozel CH",
+      userImage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjuHMx2uiEO6HsCPfKcAX-B0Y_mktbVtGYzw&usqp=CAU",
+    });  
+
+    chatRef?.current?.scrollIntoView({
+      behavior: "smooth",
+    });
+
+    setInput("");
   };
 
   return (
@@ -26,7 +34,7 @@ function ChatInput({channelName, channelId}) {
         <input 
           value={input} 
           onChange={(e) => setInput(e.target.value)}
-          placeholder={`Message #ROOM`} 
+          placeholder={`Message #${channelName}`} 
         />
         <Button hidden type='submit' onClick={sendMessage}>
           SEND
